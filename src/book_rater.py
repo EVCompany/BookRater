8f4bcc6da6d1dd9f91494a83e8659e4f9e7170bd
Egor Kurakov <egor@live.ru>
2018-05-28 12:57:11 +0300

    add chapters counter

--- src/book_rater.py
+++ src/book_rater.py
@@ -1,10 +1,14 @@
 import src.pdf_console_interface as pi
 
+PAGES_OUTPUT_TEXT = "Страниц в документе: "
+CHAPTERS_OUTPUT_TEXT = "Глав в документе: "
+PICTURES_OUTPUT_TEXT = "Рисунков в документе: "
 
 def main():
     pdf = pi.PdfConsoleInterface()
     pdf.load_file("/Users/egor/Documents/prog/python/practice/example.pdf")
-    print(pdf.get_number_of_pages())
+    print(PAGES_OUTPUT_TEXT + str(pdf.get_number_of_pages()))
+    print(CHAPTERS_OUTPUT_TEXT + str(pdf.get_number_of_chapters()))
     pdf.close_file()
 
 
