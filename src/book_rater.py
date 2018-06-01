e3549faf868600c74c970aadff4e8436177551cd
Egor Kurakov <egor@live.ru>
2018-06-01 12:48:04 +0300

    Add year & literature counter

--- src/book_rater.py
+++ src/book_rater.py
@@ -5,12 +5,16 @@ import time
 PAGES_OUTPUT_TEXT = "Страниц в документе: "
 CHAPTERS_OUTPUT_TEXT = "Глав в документе: "
 PICTURES_OUTPUT_TEXT = "Рисунков в документе: "
+YEAR_OUTPUT_TEXT = "Год: "
+LITCOUNT_OUTPUT_TEXT = "Количество использованной литературы: "
 
 def print_metrics(pdf):
     metrics = pdf.get_metrics()
     print(PAGES_OUTPUT_TEXT + str(metrics[0]) + "\n" +
           PICTURES_OUTPUT_TEXT + str(metrics[1]) + "\n" +
-          CHAPTERS_OUTPUT_TEXT + str(metrics[2]))
+          CHAPTERS_OUTPUT_TEXT + str(metrics[2]) + "\n" +
+          YEAR_OUTPUT_TEXT + str(metrics[4]) + "\n" +
+          LITCOUNT_OUTPUT_TEXT + str(metrics[3]) + "\n")
 
 def main():
     start = time.time()
